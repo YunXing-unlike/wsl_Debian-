1bdf37e03992db1f22ab4a0de86b471a8b32716c
YunXing-unlike <2153128742@qq.com>
2026-04-10 08:13:57 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -114,8 +114,6 @@ install_nodejs() {
     # 配置npm国内镜像源
     log_info "配置npm国内镜像源..."
     npm config set registry https://registry.npmmirror.com/
-    npm config set sass_binary_site https://npmmirror.com/mirrors/node-sass/
-    npm config set electron_mirror https://npmmirror.com/mirrors/electron/
     
     log_info "Node.js环境配置完成"
 }
